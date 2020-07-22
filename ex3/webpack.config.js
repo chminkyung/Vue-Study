8e6892f66003f2424c971c017b6d5e7d0b138bbc
min <min@goand.com>
2020-07-22 23:35:07 +0900

    [3-2] 프로젝트 구조와 웹팩 빌드

--- ex3/webpack.config.js
+++ ex3/webpack.config.js
@@ -1,7 +1,9 @@
+const path = require('path'); // node가 만들어둔 script
+
 // script들을 하나로 합치기 위해 webpack을 씀
 module.exports = { // node의 module을 만들었다.
     entry: {
-        app: './main.js', // app : 나중에 하나로 합쳐질 파일 이름
+        app: path.join(__dirname, 'main.js'), // app : 나중에 하나로 합쳐질 파일 이름
     },
     module: { // webpack의 핵심
         rules: [{ // 어떻게 합칠지 어떻게 처리할건지
@@ -11,6 +13,6 @@ module.exports = { // node의 module을 만들었다.
     plugins: [],
     output: {
         filename: '[name].js',
-        path: './dist', // 폴더 경로
+        path: path.join(__dirname, 'dist'), // 폴더 경로 path.join === 현재경로
     },
 };
