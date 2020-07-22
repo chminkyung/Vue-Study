a072a4c7de66aa46a99ccaaa74f0c5809f9aee40
min <min@goand.com>
2020-07-22 23:19:36 +0900

    [3-1] 웹팩 사용하기

---
+++ ex3/webpack.config.js
@@ -0,0 +1,16 @@
+// script들을 하나로 합치기 위해 webpack을 씀
+module.exports = { // node의 module을 만들었다.
+    entry: {
+        app: './main.js', // app : 나중에 하나로 합쳐질 파일 이름
+    },
+    module: { // webpack의 핵심
+        rules: [{ // 어떻게 합칠지 어떻게 처리할건지
+
+        }]
+    },
+    plugins: [],
+    output: {
+        filename: '[name].js',
+        path: './dist', // 폴더 경로
+    },
+};
